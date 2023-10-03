a00e41fe4d6d6fb3c6708e9e0ef7b8915d572274
Ozeliurs <ozeliurs@gmail.com>
2023-10-04 00:04:46 +0200

    fix: reverse the prompt

--- utils.py
+++ utils.py
@@ -21,9 +21,6 @@ async def generate_prompt(
     async for message in message.channel.history(limit=discord_message_history_count):
         message_prompt.append(f"{message.author.name}: {message.content}")
 
-    # Reverse the prompt
-    message_prompt = message_prompt[::-1]
-
     # Make the prompt a single string
     message_prompt = "\n".join(message_prompt)
 
